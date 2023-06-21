793586b1fdc81e64e579a275300518c340c4ee41
Devanshu Agarwal <agarwal.devanshu12@gmail.com>
2023-06-21 12:24:28 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,13 +1,17 @@
-function skillMember() {
-  let member = document.getElementsByClassName('member')
-  let skill = document.getElementsByClassName('skill')
-  for (let i = 0; i < member.length; i++) {
-    member[i].addEventListener('click', () => {
-      if (skill[i].style.display === 'none') {
-        skill[i].style.display = 'block'
-      } else {
-        skill[i].style.display = 'none'
-      }
-    })
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberBtn = document.getElementById("memberBtn");
+  var skillsBtn = document.getElementById("skillsBtn");
+  if (member.style.display === "none") {
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#F5F5F5";
+    skillsBtn.style.backgroundColor = "#FFFFFF";
+  } else {
+    member.style.display = "none";
+    skills.style.display = "block";
+    memberBtn.style.backgroundColor = "#FFFFFF";
+    skillsBtn.style.backgroundColor = "#F5F5F5";
   }
 }
